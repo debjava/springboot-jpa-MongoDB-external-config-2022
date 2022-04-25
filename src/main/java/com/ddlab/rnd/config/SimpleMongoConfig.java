e1d1ba43d85821e43e233d14346296837fe60554
debjava <deba.java@gmail.com>
2022-04-25 21:38:12 +0530

    Code committed and pushed by deba.java@gmail.com on 25-Apr-2022 09:38 PM using DDLAB CodePusblisher tool

---
+++ src/main/java/com/ddlab/rnd/config/SimpleMongoConfig.java
@@ -0,0 +1,30 @@
+package com.ddlab.rnd.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@Configuration
+public class SimpleMongoConfig {
+
+	private String mongoDBDatabaseName = "test";
+
+	@Bean
+	public MongoClient mongo() {
+		ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
+		MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(connectionString)
+				.build();
+
+		return MongoClients.create(mongoClientSettings);
+	}
+
+	@Bean
+	public MongoTemplate mongoTemplate() throws Exception {
+		return new MongoTemplate(mongo(), mongoDBDatabaseName);
+	}
+}
